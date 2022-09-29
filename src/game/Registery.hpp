d7c37fcf4ed305677b1d0680f2c72c48b69d119a
inv_puppet <quentin.leroueil@epitech.eu>
2022-09-29 14:02:26 +0200

    add Registery can create sparseArray and get them

--- src/game/Registery.hpp
+++ src/game/Registery.hpp
@@ -5,29 +5,30 @@
 ** Registery
 */
 
-#ifndef REGISTERY_HPP_
-#define REGISTERY_HPP_
+#pragma once
 
 #include <any>
-#include <iostream>
 #include <typeindex>
 #include <typeinfo>
 #include "SparseArray.hpp"
 #include <unordered_map>
+#include <iostream>
 
 class Registery {
   public:
     Registery();
     ~Registery();
 
-    template <class Component> SparseArray<Component> &register_component();
+    template <class Component>
+    SparseArray<Component> &register_component();
 
-    template <class Component> SparseArray<Component> &get_components();
+    template <class Component>
+    SparseArray<Component> &get_components();
 
-    template <class Component> SparseArray<Component> const &get_components() const;
+    template <class Component>
+    SparseArray<Component> const &get_components() const;
+
 
   private:
     std::unordered_map<std::type_index, std::any> _map;
 };
-
-#endif /*REGISTERY_HPP_*/
