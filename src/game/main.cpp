3ef92469df8048d8919da0badd19bc12e82c7870
Alexandre Tomasin <alexandre.tomasin@epitech.eu>
2022-09-30 16:58:34 +0200

    [ADD]: asio introduction to socket tuto1 and tuto2

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -4,5 +4,38 @@
 ** File description:
 ** main
 */
+#include <iostream>
+#include <boost/array.hpp>
+#include <asio.hpp>
 
-int main(void) { return (0); }
+using asio::ip::tcp;
+
+int main(int ac, char **av)
+{
+	try {
+		if (ac != 2) {
+			std::cerr << "Usage: client <host>" << std::endl;
+			return 1;
+		}
+		asio::io_context io_context;
+		tcp::resolver resolver(io_context);
+		tcp::resolver::results_type endpoints = resolver.resolve(av[1], "daytime");
+		tcp::socket socket(io_context);
+		asio::connect(socket, endpoints);
+		for (;;) {
+			boost::array<char, 128> buf;
+			asio::error_code error;
+
+			size_t len = socket.read_some(asio::buffer(buf), error);
+			if (error == asio::error::eof)
+				break;
+			else if (error)
+				throw asio::system_error(error);
+
+			std::cout.write(buf.data(), len);
+		}
+	} catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+	return (0);
+}
--- src/network/main.cpp
+++ src/network/main.cpp
@@ -4,5 +4,34 @@
 ** File description:
 ** main
 */
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <asio.hpp>
 
-int main(void) { return (0); }
+std::string make_daytime_string()
+{
+	using namespace std;
+	time_t now = time(0);
+	return ctime(&now);
+}
+
+int main(void)
+{
+	try {
+		asio::io_context io_context;
+		asio::ip::tcp::acceptor acceptor(io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 8080));
+		for (;;) {
+			asio::ip::tcp::socket socket(io_context);
+			acceptor.accept(socket);
+			std::string message = make_daytime_string();
+
+			asio::error_code ignored_error;
+			asio::write(socket, asio::buffer(message), ignored_error);
+		}
+	} catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}
