272b411151e08410c24563c85b90098b6afac0f5
François <francois.baudouin@epitech.eu>
2022-10-04 09:45:06 +0200

    create network communication between server and client

--- src/network/main.cpp
+++ src/network/main.cpp
@@ -5,4 +5,37 @@
 ** main
 */
 
-int main(void) { return (0); }
+#include <iostream>
+#include "boost/asio.hpp"
+#include <thread>
+using boost::asio::ip::udp;
+
+std::string read_(boost::asio::ip::tcp::socket & socket) {
+       boost::asio::streambuf buf;
+       boost::asio::read_until( socket, buf, "\n" );
+       std::string data = boost::asio::buffer_cast<const char*>(buf.data());
+       return data;
+}
+void send_(boost::asio::ip::tcp::socket & socket, const std::string& message) {
+       const std::string msg = message + "\n";
+       boost::asio::write( socket, boost::asio::buffer(message) );
+}
+
+int main(void)
+{
+    boost::asio::io_service io_service;
+    // listen for new connection
+    boost::asio::ip::tcp::acceptor acceptor_(
+        io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 1234));
+    // socket creation
+    boost::asio::ip::tcp::socket socket_(io_service);
+    // waiting for connection
+    acceptor_.accept(socket_);
+    // read operation
+    std::string message = read_(socket_);
+    std::cout << message << std::endl;
+    // write operation
+    send_(socket_, "Hello From Server!");
+    std::cout << "Servent sent Hello message to Client!" << std::endl;
+    return 0;
+}
