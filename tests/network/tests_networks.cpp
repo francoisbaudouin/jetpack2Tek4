4261fd43a957be507c94560bd0b72e4e73d3e31d
François <francois.baudouin@epitech.eu>
2022-10-06 17:55:57 +0200

    init env test

---
+++ tests/network/tests_networks.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** jetpack2Tek4
+** File description:
+** tests_networks
+*/
+
+#include <gtest/gtest.h>
+
+TEST(simple_test, return_zero)
+{
+    
+}
---
+++ tests/tests_ecs.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** Bomberman: Global Offensive
+** File description:
+** Global ECS tests
+*/
+
+#include <gtest/gtest.h>
+
+TEST(networks, readFile)
+{
+
+}
