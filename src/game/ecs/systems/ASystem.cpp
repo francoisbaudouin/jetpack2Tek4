9c05ea1330456358b8156a02a35a2741b882317c
Ugo boulestreau <ugo.boulestreau@epitech.eu>
2022-10-13 10:35:40 +0200

    removed useless iostream header in ASystem

--- src/game/ecs/systems/ASystem.cpp
+++ src/game/ecs/systems/ASystem.cpp
@@ -6,7 +6,7 @@
 */
 
 #include "ASystem.hpp"
-#include <iostream>
+
 using namespace ecs;
 
 ASystem::ASystem(std::unordered_map<size_t, std::shared_ptr<ecs::Entity>> &entityMap) : _entityMap(entityMap) {}
