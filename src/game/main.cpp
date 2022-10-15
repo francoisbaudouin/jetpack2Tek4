c0d4cf2b7ee45f9ece2700f1c7108cd1c6cdef98
inv_puppet <quentin.leroueil@epitech.eu>
2022-10-15 18:22:46 +0200

    clear main.cpp

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -5,23 +5,4 @@
 ** main
 */
 
-#include <iostream>
-#include "ecs/Ecs.hpp"
-#include "ecs/components/Position.hpp"
-#include "ecs/components/Velocity.hpp"
-#include "ecs/systems/Move.hpp"
-
-using namespace ecs;
-
-int main(void)
-{
-    Ecs ecs;
-    auto &ent = ecs.createEntity();
-    auto uEcs = std::make_shared<Ecs>(ecs);
-    Move move(uEcs);
-
-    ent.addComponent<Position>(0, 0);
-    ent.addComponent<Velocity>(0, 0);
-    move.run();
-    return (0);
-}
+int main(void) { return (0); }
