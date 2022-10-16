547837432499d54c6dc2d47c94e4845896defbd7
Ugo boulestreau <ugo.boulestreau@epitech.eu>
2022-10-16 15:58:14 +0200

    cleaned and documented ColliderReaction

--- src/game/ecs/systems/ColliderReaction.hpp
+++ src/game/ecs/systems/ColliderReaction.hpp
@@ -13,18 +13,26 @@
 #include "../components/Type.hpp"
 #include "../enums/entityTypes.hpp"
 #include "ASystem.hpp"
-#include <functional>
 
 namespace ecs
 {
     class ColliderReaction : public ASystem {
       public:
+      /**
+       * @brief Construct a new Collider Reaction that react to Collides events
+       * 
+       * @param manager shared pointer to ecs
+       */
         ColliderReaction(std::shared_ptr<Ecs> manager);
         ~ColliderReaction();
-
+        /**
+         * @brief run the ControllerReaction system that will react depending on entityType of entities given in parameters 
+         * 
+         * @param entityId1 id of the first entity
+         * @param entityId2 id of the second entity
+         */
         void run(const size_t entityId1, const size_t entityId2);
 
-      protected:
       private:
         std::map<std::pair<entityType, entityType>, void (*) (std::shared_ptr<Ecs> &manager, Entity &entity1, Entity &entity2)>
         _reactionMap;
