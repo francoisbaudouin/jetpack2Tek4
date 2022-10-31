21e10d1ea5ce80663c4159723c58329ad8b04b3d
François <francois.baudouin@epitech.eu>
2022-10-31 08:48:41 +0100

    add asio included file

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -8,6 +8,7 @@
 #include "Client.hpp"
 #include "ecs/systems/Display.hpp"
 #include "ecs/systems/Input.hpp"
+#include <string>
 
 using namespace rtype;
 
@@ -32,11 +33,12 @@ void Client::connectToServer()
 void Client::run(boost::asio::ip::udp::socket &socket)
 {
     sf::Event event;
-    size_t messageLength = 0;
+    (void) socket;
+    //size_t messageLength = 0;
 
     while (_window.isOpen()) {
         // fonction pour envoyer des infos au serveur à mettre ici
-        messageLength = socket.receive_from(boost::asio::buffer(this->_receiveBuffer), this->_senderEndpoint);
+        //messageLength = socket.receive_from(boost::asio::buffer(this->_receiveBuffer), this->_senderEndpoint);
         // fonction qui désérialise les infos reçues par le server à mettre ici
         // update ecs côté client
         _window.clear();
