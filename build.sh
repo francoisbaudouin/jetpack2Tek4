ec4d1458bbd187c9d45679b6a2cc43ef4af04f42
François <francois.baudouin@epitech.eu>
2022-11-07 16:46:57 +0100

    set compiler for windows

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ mkdir build
 cd build
 conan install ../. --build=missing  -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 cd ..
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake -G "MSVC" -DCMAKE_BUILD_TYPE=Release
 cmake --build . -j
