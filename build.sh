e081f31506f0f8e42eb172e1f0a8550aa2f0ab73
François <francois.baudouin@epitech.eu>
2022-11-09 23:46:50 +0100

    fix bad generators implementation

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ mkdir build
 cd build
 conan install ../. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 cd ..
-cmake -G " * Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release
+cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release
 cmake --build . -j
