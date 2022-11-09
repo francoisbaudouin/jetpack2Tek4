65dd36d355d1cfd8fd149e8cfb66f6ec8869e8ef
François <francois.baudouin@epitech.eu>
2022-11-09 22:07:21 +0100

    add set provide compiler

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ mkdir build
 cd build
 conan install ../. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 cd ..
-cmake -G "Visual Studio 2019" -DCMAKE_BUILD_TYPE=Release
+cmake -G " * Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Release
 cmake --build . -j
