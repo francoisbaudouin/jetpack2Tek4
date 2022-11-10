13246a0327523edddf8b1db4735608c3eef31f46
Alexandre Tomasin <alexandre.tomasin@epitech.eu>
2022-11-10 15:08:02 +0100

    fix lint format

--- src/server/Test.cpp
+++ src/server/Test.cpp
@@ -7,8 +7,6 @@
 
 #include "Test.hpp"
 
-Test::Test(const std::string &name, int value) : _name(name), _value(value)
-{
-}
+Test::Test(const std::string &name, int value) : _name(name), _value(value) {}
 
 Test::~Test() {}
