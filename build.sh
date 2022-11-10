fad5c79aa5fb12b8cfff477261b4447e577fb79a
François <francois.baudouin@epitech.eu>
2022-11-10 13:52:49 +0100

    rework generators file

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ mkdir build
 cd build
 conan install ../. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 cd ..
-#cmake -B build/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=build/conan_paths.cmake
-cmake -G "Visual Studio" -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=build/conan_paths.cmake
+cmake -B build/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=build/conan_paths.cmake
+#cmake -G "Visual Studio" -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=build/conan_paths.cmake
+cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=build/conan_paths.cmake
 #cmake --config Release --build build/ --target install
 cmake --build . -j
