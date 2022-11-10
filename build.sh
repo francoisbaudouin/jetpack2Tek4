a0c4c5f9bd234f2e4fbdfb5b9e3b6c5bf0f52fa0
François <francois.baudouin@epitech.eu>
2022-11-10 12:13:12 +0100

    fix vs not found

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 pip3 install conan
 mkdir build
 cd build
-conan install ../. -s compiler="Visual Studio" -s compiler.version=15 -s arch=x86_64 --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
+conan install ../. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 cd ..
 #cmake -B build/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=build/conan_paths.cmake
 cmake -G "Visual Studio" -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=build/conan_paths.cmake
