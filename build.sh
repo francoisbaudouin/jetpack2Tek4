bf960143441a0e24338557596df5c61c70fa2c7e
François <francois.baudouin@epitech.eu>
2022-11-10 11:43:26 +0100

    rework implementation msvc

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 pip3 install conan
 mkdir build
 cd build
-conan install ../. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
+conan install ../. -s compiler="Visual Studio" -s compiler.version=15 -s arch=x86_64 --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 cd ..
 #cmake -B build/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=build/conan_paths.cmake
-#cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release --msvc
-cmake --config Release --build build/ --target install
+cmake -G "Visual Studio 15 2017" -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=build/conan_paths.cmake
+#cmake --config Release --build build/ --target install
 cmake --build . -j
